b7a0dbfcf1c471ff762194ccfc519d05072de71c
mangroa <alan.mangroo@gmail.com>
2017-09-04 09:15:12 +1000

    update

--- basicS3Busket/createstack.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-aws cloudformation create-stack --stack-name myVPCStack --template-body file:///Users/alanmangroo/IdeaProjects/cloudformation/vpcwithec2/SimpleVPC.yaml --parameters ParameterKey=InstanceNameParameter,ParameterValue=ThisEc2
--- basicS3Busket/updatestack.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-aws cloudformation update-stack --stack-name myVPCStack --template-body file:///Users/alanmangroo/IdeaProjects/cloudformation/vpcwithec2/SimpleVPC.yaml
-
